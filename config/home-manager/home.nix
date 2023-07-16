54d8e043155addd61848cba12a25157df0a0897e
Stephen Huan <stephen03.huan@gmail.com>
2023-07-16 01:26:20 -0700

    feat(packages): htop, pulsemixer, tokei, cython

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -18,9 +18,11 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.htop
+    pkgs.pulsemixer
+    pkgs.tokei
+
+    pkgs.python311Packages.cython
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
