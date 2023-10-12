129c36cf6430215c5605b8907b77d10ec62f4623
Stephen Huan <stephen03.huan@gmail.com>
2023-10-12 04:34:26 -0400

    feat(nixos): enable pipewire audio/bluetooth

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -74,8 +74,15 @@
   # services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    wireplumber.enable = true;
+    pulse.enable = true;
+  };
+  hardware.bluetooth.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
