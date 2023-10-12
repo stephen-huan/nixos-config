85408c84d40ef47aa485e3ddfbdb99800cf6d8e6
Stephen Huan <stephen03.huan@gmail.com>
2023-10-11 23:03:15 -0400

    fix(config/home-manager): change username

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "stephenhuan";
-  home.homeDirectory = "/home/stephenhuan";
+  home.username = "ikue";
+  home.homeDirectory = "/home/ikue";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
