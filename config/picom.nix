ec24ad2abb3cf30e09aae2d48095718ada3f19d0
Stephen Huan <stephen03.huan@gmail.com>
2023-10-11 23:30:03 -0400

    feat(config/picom): add picom config

---
+++ config/picom.nix
@@ -0,0 +1,19 @@
+{
+  services.picom = {
+    enable = true;
+    # use OpenGL as the rendering backend
+    backend = "glx";
+    # without fading, some screen tears
+    fade = true;
+    # speed up default fade speed
+    fadeDelta = 3;
+    # make inactive windows slightly transparent
+    inactiveOpacity = 0.9;
+    opacityRules = [
+        # exclude screensaver (i3lock) window
+        "100:class_g = 'i3lock'"
+        # exclude floating windows
+        "100:I3_FLOATING_WINDOW@:c"
+    ];
+  };
+}
