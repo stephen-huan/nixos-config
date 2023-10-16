32c035aa0f6d7111353bda7701d20538c9e26284
Stephen Huan <stephen03.huan@gmail.com>
2023-10-16 14:12:10 -0400

    refactor(home-manager/persistence): /persistent

--- config/home-manager/persistence.nix
+++ config/home-manager/persistence.nix
@@ -1,7 +1,10 @@
 { config, ... }:
 
+let
+  homeDir = "${config._module.args.persistent}${config.home.homeDirectory}";
+in
 {
-  home.persistence."/persistent${config.home.homeDirectory}" = {
+  home.persistence.${homeDir} = {
     allowOther = true;
     # uses `bindfs` (https://bindfs.org/) rather than `mount --bind`
     directories = [ ];
