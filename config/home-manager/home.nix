8d4c776e0cf49725e4d0e592f9388fb01de06152
Stephen Huan <stephen03.huan@gmail.com>
2023-10-17 02:06:22 -0400

    style(config/home-manager/home.nix): reflow

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 
 {
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home = {
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager
+    # release introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you
+    # do want to update the value, then make sure to first check the Home
+    # Manager release notes.
     stateVersion = "23.05"; # Please read the comment before changing.
 
     # The home.packages option allows you to install Nix packages into your
@@ -74,10 +74,10 @@
 
       pkgs.python311Packages.cython
 
-      # # It is sometimes useful to fine-tune packages, for example, by applying
-      # # overrides. You can do that directly here, just don't forget the
-      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-      # # fonts?
+      # # It is sometimes useful to fine-tune packages, for example, by
+      # # applying overrides. You can do that directly here, just don't forget
+      # # the parentheses. Maybe you want to install Nerd Fonts with a limited
+      # # number of fonts?
       # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
       # # You can also create simple shell scripts directly inside your
@@ -88,8 +88,8 @@
       # '')
     ] ++ (import ./unfree.nix pkgs);
 
-    # Home Manager is pretty good at managing dotfiles. The primary way to manage
-    # plain files is through 'home.file'.
+    # Home Manager is pretty good at managing dotfiles. The primary way to
+    # manage plain files is through 'home.file'.
     file = {
       ".config/ames/config".source = ../ames/config;
 
