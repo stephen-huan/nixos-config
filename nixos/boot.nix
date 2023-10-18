dae5a0f61bf54eafd0adecab3c95d29a5aaeacd5
Stephen Huan <stephen03.huan@gmail.com>
2023-10-18 06:53:48 -0400

    feat(nixos/boot.nix): replace grub by systemd-boot

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -3,13 +3,9 @@
     kernelParams = [ "boot.shell_on_fail" ];
     loader = {
       efi.canTouchEfiVariables = false;
-      grub = {
-        device = "nodev";
-        efiSupport = true;
-        # https://github.com/NixOS/nixpkgs/issues/243026
-        efiInstallAsRemovable = true;
-        extraGrubInstallArgs = [ "--disable-shim-lock" ];
-        splashImage = import ./figures/splashImage.nix;
+      systemd-boot = {
+        enable = true;
+        editor = false;
       };
     };
   };
