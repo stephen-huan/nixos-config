33ae7d847edbdfc025d9b31a3f7d49dc66c0e0c0
Stephen Huan <stephen03.huan@gmail.com>
2023-10-21 18:55:05 -0400

    feat(nixos/networking.nix): internal iwd dhcp

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -7,11 +7,18 @@
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
     hostName = config._module.args.hostname;
     firewall.enable = true;
-    wireless.iwd.enable = true;
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        General.EnableNetworkConfiguration = true;
+        Network.NameResolvingService = "resolvconf";
+      };
+    };
     useDHCP = false;
     interfaces = {
       eno1.useDHCP = true;
-      wlan0.useDHCP = true;
+      # configured by iwd
+      wlan0.useDHCP = false;
     };
     dhcpcd = {
       enable = true;
