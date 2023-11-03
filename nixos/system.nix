23a78d562de320fa2786de8bb9eaed21790b27f7
Stephen Huan <stephen03.huan@gmail.com>
2023-11-03 06:29:56 -0400

    refactor(nixos/system.nix): move stateVersion

--- nixos/system.nix
+++ nixos/system.nix
@@ -1,6 +1,21 @@
 { specialArgs, ... }:
 
 {
-  # nixos-version --configuration-revision
-  system.configurationRevision = specialArgs.self.rev or "dirty";
+  system = {
+    # nixos-version --configuration-revision
+    configurationRevision = specialArgs.self.rev or "dirty";
+
+    # Copy the NixOS configuration file and link it from the resulting system
+    # (/run/current-system/configuration.nix). This is useful in case you
+    # accidentally delete configuration.nix.
+    # copySystemConfiguration = true;
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It's perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    stateVersion = "23.11"; # Did you read the comment?
+  };
 }
