4cdf2966b08661d0b47cb7f9b46455c794c8108c
Stephen Huan <stephen03.huan@gmail.com>
2023-11-03 17:19:49 -0400

    fix(nixos/nixpkgs.nix): allow insecure zotero

--- nixos/nixpkgs.nix
+++ nixos/nixpkgs.nix
@@ -1,5 +1,10 @@
 { self, ... }:
 
 {
-  nixpkgs.overlays = builtins.attrValues self.overlays;
+  nixpkgs = {
+    overlays = builtins.attrValues self.overlays;
+    config = {
+      permittedInsecurePackages = [ "zotero-6.0.27" ];
+    };
+  };
 }
