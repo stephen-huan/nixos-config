7b23f530dff5cea1dd4e82048dd054ecbcb9577a
Stephen Huan <stephen03.huan@gmail.com>
2023-11-06 05:28:35 -0500

    refactor(persistence): remove explicit .config

--- config/home-manager/persistence.nix
+++ config/home-manager/persistence.nix
@@ -1,23 +1,29 @@
 { config, lib, ... }:
 
+let
+  strip = lib.stripPrefix config.home.homeDirectory;
+  configHome = strip config.xdg.configHome;
+in
 {
   home.persistence.${lib.persistentHome config} = {
     allowOther = true;
     # uses `bindfs` (https://bindfs.org/) rather than `mount --bind`
     directories = map (directory: { inherit directory; method = "symlink"; }) [
       "bin"
-      ".config/Cider"
-      ".config/cmus"
-      ".config/home-manager"
-      ".config/memento"
-      ".config/Signal"
       ".julia"
       "not-programs"
       ".password-store"
       "programs"
       ".zotero"
       "Zotero"
+    ] ++ map
+      (name: { directory = "${configHome}/${name}"; method = "symlink"; }) [
+      "Cider"
+      "cmus"
+      "home-manager"
+      "memento"
+      "Signal"
     ];
-    files = [ ".config/mimeapps.list" ];
+    files = [ "${configHome}/mimeapps.list" ];
   };
 }
--- lib/default.nix
+++ lib/default.nix
@@ -1,11 +1,26 @@
 { pkgs }:
 
+let
+  lib = pkgs.lib;
+in
 {
+  # loads *.nix files and directories with default.nix
   importDir = path: {
-    # loads *.nix files and directories with default.nix
     imports = map (name: ../${path}/${name})
       (builtins.attrNames (builtins.readDir ../${path}));
   };
+  # return the persistent home directory
   persistentHome = config:
     "${config._module.args.persistent}${config.home.homeDirectory}";
+  # remove prefix from a path, if it exists
+  stripPrefix = prefix: path:
+    let
+      start = lib.splitString "/" prefix;
+      tokens = lib.splitString "/" path;
+      n = builtins.length start;
+      join = builtins.concatStringsSep "/";
+    in
+    if lib.take n tokens == start
+    then join (lib.drop n tokens)
+    else join tokens;
 }
