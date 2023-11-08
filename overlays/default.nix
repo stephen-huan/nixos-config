ca1c96ea7a1e342a60e943665a5a59dedf62ff37
Stephen Huan <stephen03.huan@gmail.com>
2023-11-08 14:15:53 -0500

    feat(overlays/ibus-engines.nix): add mozc-ut

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,4 +3,5 @@
   lib = final: prev: { lib = prev.lib // (import ../lib { pkgs = final; }); };
   maintainers = import ./maintainers.nix;
   fishPlugins = import ./fishPlugins.nix;
+  ibus-engines = import ./ibus-engines.nix;
 }
---
+++ overlays/ibus-engines.nix
@@ -0,0 +1,21 @@
+final: prev:
+
+let
+  self = prev.ibus-engines;
+  mozcdic = final.mozcdic-ut.override {
+    alt-cannadic = true;
+    edict = true;
+    skk-jisyo = true;
+    sudachidict = true;
+  };
+in
+{
+  ibus-engines = self // {
+    mozc-ut = self.mozc.overrideAttrs {
+      preBuild = ''
+        # Add the UT dictionary
+        cat ${mozcdic}/mozcdic-ut.txt >> data/dictionary_oss/dictionary00.txt
+      '';
+    };
+  };
+}
