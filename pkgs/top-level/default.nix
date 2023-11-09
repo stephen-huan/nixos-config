ebbcdd67292bc63f30120c5780ce8474128b2dc8
Stephen Huan <stephen03.huan@gmail.com>
2023-11-09 04:41:05 -0500

    feat(pkgs): add kanjistrokeorders font

--- pkgs/top-level/default.nix
+++ pkgs/top-level/default.nix
@@ -6,4 +6,5 @@ in
 {
   iwd-last-network = callPackage ../os-specific/linux/iwd-last-network { };
   mozcdic-ut = callPackage ../tools/inputmethods/mozcdic-ut { };
+  kanjistrokeorders = callPackage ../data/fonts/kanjistrokeorders { };
 }
