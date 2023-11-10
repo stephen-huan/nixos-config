7129e39504a9220aa06e48509dd270d48ff82aa6
Stephen Huan <stephen03.huan@gmail.com>
2023-11-10 05:34:08 -0500

    fix(config/nixpkgs): add flake overlays

--- config/nixpkgs.nix
+++ config/nixpkgs.nix
@@ -1,10 +1,13 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, self, ... }:
 
 let
   unfreePkgs = map lib.getName (import ./home-manager/unfree.nix pkgs);
 in
 {
-  nixpkgs.config = {
-    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) unfreePkgs;
+  nixpkgs = {
+    overlays = builtins.attrValues self.overlays;
+    config = {
+      allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) unfreePkgs;
+    };
   };
 }
