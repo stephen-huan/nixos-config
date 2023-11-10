320b538c859a1556a0d332cf7e7aa0cde840cc31
Stephen Huan <stephen03.huan@gmail.com>
2023-11-10 05:34:29 -0500

    feat(config/nixpkgs): add insecure packages

---
+++ config/home-manager/insecure.nix
@@ -0,0 +1,5 @@
+{ pkgs, ... }:
+# a list of insecure packages to whitelist
+[
+  "zotero-6.0.27"
+]
--- config/nixpkgs.nix
+++ config/nixpkgs.nix
@@ -2,12 +2,14 @@
 
 let
   unfreePkgs = map lib.getName (import ./home-manager/unfree.nix pkgs);
+  permittedInsecurePackages = import ./home-manager/insecure.nix pkgs;
 in
 {
   nixpkgs = {
     overlays = builtins.attrValues self.overlays;
     config = {
       allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) unfreePkgs;
+      inherit permittedInsecurePackages;
     };
   };
 }
