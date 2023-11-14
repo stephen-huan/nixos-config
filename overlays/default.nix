57a1dbd8e219efc6bc5fe01605db594f84f4b642
Stephen Huan <stephen03.huan@gmail.com>
2023-11-14 05:28:23 -0500

    feat(overlays): fix backintime-qt_polkit

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,7 @@
   pkgs = final: prev: import ../pkgs/top-level { pkgs = final; };
   lib = final: prev: { lib = prev.lib // (import ../lib { pkgs = final; }); };
   maintainers = import ./maintainers.nix;
+  backintime = import ./backintime.nix;
   fishPlugins = import ./fishPlugins.nix;
   ibus-engines = import ./ibus-engines.nix;
   vimPlugins = import ./vimPlugins.nix;
