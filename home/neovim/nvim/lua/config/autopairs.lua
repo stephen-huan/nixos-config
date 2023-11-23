f164c82898b6a2f30d7bb402b50d55822902d15e
Stephen Huan <stephen03.huan@gmail.com>
2023-11-23 06:19:15 -0500

    feat(neovim/autopairs): paired latex delimiters

--- home/neovim/nvim/lua/config/autopairs.lua
+++ home/neovim/nvim/lua/config/autopairs.lua
@@ -2,16 +2,15 @@ local npairs = require "nvim-autopairs"
 local Rule = require "nvim-autopairs.rule"
 
 local latex = { "tex", "latex" }
--- stylua: ignore start
 local rules = {
-    Rule("\\(", "  \\)", latex)
-        :set_end_pair_length(3),
-    Rule("\\[", "  \\]", latex)
-        :set_end_pair_length(3),
-    Rule("$", "  $", "markdown")
-        :set_end_pair_length(2),
+    Rule("\\(", "  \\)", latex):set_end_pair_length(3),
+    Rule("\\[", "  \\]", latex):set_end_pair_length(3),
+    Rule("\\{", "  \\}", latex):set_end_pair_length(3),
+    Rule("\\left(", "  \\right)", latex):set_end_pair_length(8),
+    Rule("\\left[", "  \\right]", latex):set_end_pair_length(8),
+    Rule("\\left\\{", "  \\right\\}", latex):set_end_pair_length(9),
+    Rule("$", "  $", "markdown"):set_end_pair_length(2),
 }
--- stylua: ignore end
 
 for _, rule in pairs(rules) do
     npairs.add_rule(rule)
