15f780526d326b55c797d366bc0e8ba4da7b2cb6
Stephen Huan <stephen03.huan@gmail.com>
2023-11-26 12:32:25 -0500

    fix(home/caffeine): remove persistence

--- home/caffeine.nix
+++ home/caffeine.nix
@@ -1,13 +1,7 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
-let
-  configHome = lib.stripPrefix config.home.homeDirectory config.xdg.configHome;
-in
 {
   home.packages = [ pkgs.caffeine-ng ];
   # launched by xdg autostart
   services.caffeine.enable = false;
-  home.persistence.${lib.persistentHome config}.directories = [
-    { directory = "${configHome}/caffeine"; method = "symlink"; }
-  ];
 }
