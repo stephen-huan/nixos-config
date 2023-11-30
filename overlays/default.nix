4178d95275c4567296fee278dcd4d066736c102d
Stephen Huan <stephen03.huan@gmail.com>
2023-11-30 08:14:53 -0500

    fix(overlays/backintime): remove backintime

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@
   pkgs = final: prev: import ../pkgs/top-level { pkgs = final; };
   lib = final: prev: { lib = prev.lib // (import ../lib { pkgs = final; }); };
   maintainers = import ./maintainers.nix;
-  backintime = import ./backintime.nix;
   fishPlugins = import ./fishPlugins.nix;
   ibus-engines = import ./ibus-engines.nix;
   vimPlugins = import ./vimPlugins.nix;
