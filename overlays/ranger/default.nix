9bbbecba8ffed66ec16ec06a5278d2bf89f468e4
Stephen Huan <stephen03.huan@gmail.com>
2023-12-22 04:42:46 -0800

    feat(overlays/ranger): remove /bin/sh from ranger

--- overlays/ranger/default.nix
+++ overlays/ranger/default.nix
@@ -2,9 +2,17 @@ final: prev:
 
 let
   self = prev.ranger;
+  sh = "${final.busybox-sandbox-shell}/bin/sh";
 in
 {
-  ranger = self.overrideAttrs {
+  ranger = self.overrideAttrs (previousAttrs: {
     patches = [ ./lazy-nix-store.patch ];
-  };
+
+    preConfigure = previousAttrs.preConfigure + ''
+      substituteInPlace \
+        ranger/config/commands.py \
+        ranger/ext/rifle.py \
+        --replace "/bin/sh" "${sh}"
+    '';
+  });
 }
