27da97e6814eccfd8711f8cda76881547dedcf5a
Stephen Huan <stephen03.huan@gmail.com>
2023-12-22 20:56:23 -0800

    feat(overlays/nobinsh): add xorg.xrdb

--- overlays/nobinsh.nix
+++ overlays/nobinsh.nix
@@ -9,13 +9,24 @@ let
 
     i3 = self.callPackage prev.i3.override { };
     xlayoutdisplay = self.callPackage prev.xlayoutdisplay.override { };
+    xrdb = self.callPackage prev.xorg.xrdb.override { };
+    xorg = final.xorg // {
+      inherit (self)
+        xrdb;
+    };
   });
+  addFlags = pkgs: builtins.mapAttrs
+    # https://github.com/NixOS/nixpkgs/issues/129595#issuecomment-897979569
+    (_: pkg: pkg.overrideAttrs { NIX_CFLAGS_COMPILE = "-isysroot nowhere"; })
+    pkgs;
 in
-builtins.mapAttrs
-  # https://github.com/NixOS/nixpkgs/issues/129595#issuecomment-897979569
-  (_: pkg: pkg.overrideAttrs { NIX_CFLAGS_COMPILE = "-isysroot nowhere"; })
-{
+(addFlags {
   inherit (packages)
     i3
     xlayoutdisplay;
+}) // {
+  xorg = prev.xorg // (addFlags {
+    inherit (packages.xorg)
+      xrdb;
+  });
 }
