00a6ef71548e2e67c812685aeab4f44ce5d32581
Stephen Huan <stephen03.huan@gmail.com>
2023-12-24 20:45:45 -0800

    fix(home/nix): disable flake registries

--- home/nix.nix
+++ home/nix.nix
@@ -2,6 +2,8 @@
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      flake-registry = "";
+      use-registries = false;
       keep-outputs = true;
       use-xdg-base-directories = true;
     };
