7e32e9ce4cf78ac8e3a0ac6e0a1033911482d43b
Stephen Huan <stephen03.huan@gmail.com>
2024-03-18 03:42:18 -0400

    feat(nvim/plugins/vimtex): add tectonic

--- home/neovim/nvim/lua/plugins/vimtex.lua
+++ home/neovim/nvim/lua/plugins/vimtex.lua
@@ -2,9 +2,19 @@
 vim.g.tex_flavor = "latex"
 -- set PDF viewer
 vim.g.vimtex_view_method = "sioyek"
--- vim.g.vimtex_view_sioyek_exe = "sioyek"
 -- automatically close quickfix menu
 vim.g.vimtex_quickfix_autoclose_after_keystrokes = 3
+-- compilers
+vim.g.vimtex_compiler_method = "tectonic"
+vim.g.vimtex_compiler_tectonic = {
+    options = {
+        "--keep-logs",
+        "--synctex",
+        "-Z shell-escape",
+        -- breaks synctex
+        -- "-Z deterministic-mode",
+    },
+}
 vim.g.vimtex_compiler_latexmk = {
     executable = "latexmk",
     options = {
