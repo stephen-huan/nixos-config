336c02260a780f51f58f3dfd344930cff640bba9
Stephen Huan <stephen03.huan@gmail.com>
2024-03-30 17:43:53 -0400

    fix(home/gpg): pinentryFlavor -> pinentryPackage

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,9 +1,11 @@
+{ pkgs, ... }:
+
 {
   programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentryFlavor = "qt";
+    pinentryPackage = pkgs.pinentry-qt;
     # 30 minutes, resets with usage
     defaultCacheTtl = 1800;
     # 2 hours, maximum
