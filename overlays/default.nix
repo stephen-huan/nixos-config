f066d91297a6882bd50bb844eb7c6736ee190cb9
Stephen Huan <stephen03.huan@gmail.com>
2024-04-24 03:36:15 -0400

    fix(overlays/maintainers): remove

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,6 @@
 {
   pkgs = final: prev: import ../pkgs/top-level { pkgs = final; };
   lib = final: prev: { lib = prev.lib // (import ../lib { pkgs = final; }); };
-  maintainers = import ./maintainers.nix;
   caffeine-ng = import ./caffeine-ng.nix;
   fishPlugins = import ./fishPlugins.nix;
   gcc' = import ./gcc.nix;
