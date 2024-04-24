48b1386f4ce7ab8224c5cac043ac77c8c8a9af33
Stephen Huan <stephen03.huan@gmail.com>
2024-04-24 02:34:41 -0400

    Revert "inflow: init at 1.0.1"
    This reverts commit 6702bce6eadf13b58eba56b36cdfccd251359363.

--- pkgs/top-level/default.nix
+++ pkgs/top-level/default.nix
@@ -3,8 +3,7 @@
 let
   inherit (pkgs) callPackage;
 in
-rec {
-  inflow = callPackage ../by-name/in/inflow/package.nix { inherit inflow; };
+{
   iwd-last-network = callPackage ../os-specific/linux/iwd-last-network { };
   kanji-stroke-order-font = callPackage ../data/fonts/kanji-stroke-order-font
     { };
