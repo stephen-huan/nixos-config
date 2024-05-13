033df253bf68790bd07dcddba0cdc8d824e2cfba
Stephen Huan <stephen03.huan@gmail.com>
2024-05-13 13:50:06 -0700

    feat(overlays/sioyek): latest development version

--- overlays/sioyek.nix
+++ overlays/sioyek.nix
@@ -2,22 +2,17 @@ final: prev:
 
 let
   self = prev.sioyek;
-in
-{
-  sioyek = self.overrideAttrs {
+  sioyek' = self.overrideAttrs (previousAttrs: {
     src = final.fetchFromGitHub {
       owner = "ahrm";
       repo = "sioyek";
-      rev = "d2663366437413159e27a1e14700e6ff85fa47c1";
-      sha256 = "sha256-GfwOa6tNeCh1QmK3LEmGmPrcBv4sbdzU2xTcDzFxEGw=";
+      rev = "b312fe0b7ae4b8125febef4467480367e0030665";
+      sha256 = "sha256-0UCFp52mSEHFuH50FD+CrvmiR9SWfgqm8gUypX7gLG4=";
     };
-    patches = [
-      # Fixed compatibility with mupdf-0.23.0
-      # https://github.com/ahrm/sioyek/issues/804
-      (final.fetchurl {
-        url = "https://aur.archlinux.org/cgit/aur.git/plain/sioyek-mupdf-1.23.patch?h=sioyek-git&id=e4a1a281e737337bfb48392a8026f3a17cbfb86d";
-        hash = "sha256-MAIWRv2kXECvRXkGfMsJi/9LKTT/QqznsCph/RyLxXM=";
-      })
-    ];
-  };
+    buildInputs = previousAttrs.buildInputs ++ [ final.qt6.qtspeech ];
+    patches = [ ];
+  });
+in
+{
+  sioyek = final.qt6.callPackage sioyek'.override { };
 }
