967abe8b6821749210722e90b9eba6cac4ff5973
Stephen Huan <stephen03.huan@gmail.com>
2024-06-06 02:43:24 -0400

    refactor(home/xlayoutdisplay): use keyValue

--- home/xlayoutdisplay.nix
+++ home/xlayoutdisplay.nix
@@ -3,10 +3,12 @@
 {
   home = {
     packages = [ pkgs.xlayoutdisplay ];
-    file.".xlayoutdisplay".text = ''
-      # restrict frame rate to at most 60hz
-      # tuxedo pulse 15 has problems with 1440p at high refresh rates
-      rate=60
-    '';
+    file.".xlayoutdisplay" = {
+      source = (pkgs.formats.keyValue { }).generate ".xlayoutdisplay" {
+        # restrict frame rate to at most 60hz
+        # tuxedo pulse 15 has problems with 1440p at high refresh rates
+        rate = 60;
+      };
+    };
   };
 }
