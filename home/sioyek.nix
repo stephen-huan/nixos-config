42f93a840cfd07d5d0ddeec47e39a10c0fd418cd
Stephen Huan <stephen03.huan@gmail.com>
2024-06-06 02:45:40 -0400

    style(home/sioyek): lowercase comments

--- home/sioyek.nix
+++ home/sioyek.nix
@@ -2,11 +2,11 @@
   programs.sioyek = {
     enable = true;
     config = {
-      # Configure the appearance of page separator
+      # configure the appearance of page separator
       # setting to 1 will sometimes not render in specific places
       page_separator_width = "2";
       page_separator_color = "0.9 0.9 0.9";
-      # If set, we open a new sioyek window when a new file is opened,
+      # if set, we open a new sioyek window when a new file is opened,
       # otherwise we open the file in the previous window
       should_launch_new_window = "1";
     };
