bb90fb882e5954468c5158f7bc776d86157ae80f
Stephen Huan <stephen03.huan@gmail.com>
2024-06-08 06:15:52 -0400

    fix(pkgs): remove kanji-stroke-order-font

--- pkgs/top-level/default.nix
+++ pkgs/top-level/default.nix
@@ -5,8 +5,6 @@ let
 in
 {
   iwd-last-network = callPackage ../os-specific/linux/iwd-last-network { };
-  kanji-stroke-order-font = callPackage ../data/fonts/kanji-stroke-order-font
-    { };
   mozcdic-ut = callPackage ../tools/inputmethods/mozcdic-ut {
     alt-cannadic = true;
     edict = true;
