b14c707eecce0f70fbe4db7ca740f20ee48dc153
Stephen Huan <stephen03.huan@gmail.com>
2024-06-10 18:05:42 -0400

    feat(home/neovim/autocmds): syntax highlight warn

--- home/neovim/nvim/lua/autocmds.lua
+++ home/neovim/nvim/lua/autocmds.lua
@@ -59,3 +59,20 @@ vim.api.nvim_create_autocmd("FileType", {
         end
     end,
 })
+-- warning when syntax highlighting falls through
+vim.api.nvim_create_autocmd({ "BufWinEnter", "WinNew", "BufFilePost" }, {
+    group = "vimrc",
+    pattern = "*",
+    callback = function(args)
+        if
+            vim.b[args.buf].ts_highlight ~= nil
+            and not vim.b[args.buf].ts_highlight
+        then
+            vim.cmd(
+                'echohl WarningMsg | unsilent echom "'
+                    .. vim.bo[args.buf].filetype
+                    .. ' syntax highlighting disabled!" | echohl None'
+            )
+        end
+    end,
+})
