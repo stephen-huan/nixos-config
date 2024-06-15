2b4f0f0239bdfb59c7dce989296982083ff026da
Stephen Huan <stephen.huan@cgdct.moe>
2024-06-15 14:49:10 -0400

    fix(home/neovim/plugins/undotree): sometimes empty

--- home/neovim/nvim/lua/plugins/undotree.lua
+++ home/neovim/nvim/lua/plugins/undotree.lua
@@ -1,4 +1,14 @@
 -- open on right side
 vim.g.undotree_WindowLayout = 3
 -- keybindings
-vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>")
+-- HACK: https://github.com/mbbill/undotree/issues/129#issuecomment-1113252887
+vim.keymap.set("n", "<leader>u", function()
+    vim.cmd "UndotreeShow"
+    if vim.fn.getreg "%" == "undotree_2" then
+        vim.cmd "wincmd w"
+        vim.cmd "wincmd w"
+        vim.cmd "wincmd p"
+    else
+        vim.cmd "UndotreeFocus"
+    end
+end)
