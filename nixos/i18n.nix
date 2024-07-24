6d8da55b265a7eda555fe19eaa3058a9a9896536
Stephen Huan <stephen.huan@cgdct.moe>
2024-07-23 23:13:18 -0400

    refactor(nixos/i18n.nix): remove enabled option

--- nixos/i18n.nix
+++ nixos/i18n.nix
@@ -5,7 +5,8 @@
     defaultLocale = "en_US.UTF-8";
     supportedLocales = lib.mkOptionDefault [ "ja_JP.UTF-8/UTF-8" ];
     inputMethod = {
-      enabled = "ibus";
+      enable = true;
+      type = "ibus";
       ibus.engines = with pkgs.ibus-engines; [ mozc-ut ];
     };
   };
