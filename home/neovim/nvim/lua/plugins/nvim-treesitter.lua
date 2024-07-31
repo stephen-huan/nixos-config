23861ed89a467feac107e1912a21c0904193a788
Stephen Huan <stephen.huan@cgdct.moe>
2024-07-31 02:54:05 -0400

    fix(neovim/nvim-treesitter): disable nix indent

--- home/neovim/nvim/lua/plugins/nvim-treesitter.lua
+++ home/neovim/nvim/lua/plugins/nvim-treesitter.lua
@@ -15,7 +15,7 @@ require("nvim-treesitter.configs").setup {
     },
     indent = {
         enable = true,
-        disable = { "julia" },
+        disable = { "julia", "nix" },
     },
     matchup = {
         enable = true,
