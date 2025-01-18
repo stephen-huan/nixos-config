be4522d31e3fc197bcda44f1f82285ba2536c4d4
Stephen Huan <stephen.huan@cgdct.moe>
2025-01-18 13:10:33 -0500

    feat(nixos/tetuya/hardware): add swapfile

--- nixos/tetuya/hardware.nix
+++ nixos/tetuya/hardware.nix
@@ -1,5 +1,15 @@
+{ config, ... }:
+
+let
+  inherit (config._module.args) persistent;
+in
 {
   hardware = {
     cpu.intel.updateMicrocode = true;
   };
+  swapDevices = [{
+    device = "${persistent}/var/lib/swapfile";
+    size = 32 * 1024;
+    randomEncryption.enable = true;
+  }];
 }
