ba20341fdd1c1ee3c5c32f731981442a4bb8040e
Stephen Huan <stephen.huan@cgdct.moe>
2025-01-18 23:34:42 -0500

    feat(nixos/tetuya/oom): enable earlyoom

---
+++ nixos/tetuya/oom.nix
@@ -0,0 +1,9 @@
+{
+  services.earlyoom = {
+    enable = true;
+    enableNotifications = true;
+    # kill as soon as swap is used
+    freeSwapThreshold = 100;
+    freeSwapKillThreshold = 100;
+  };
+}
