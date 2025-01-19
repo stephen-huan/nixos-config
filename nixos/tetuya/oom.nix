fc20f9cd58c97400fa2830e7db67bfcfe34b1fb6
Stephen Huan <stephen.huan@cgdct.moe>
2025-01-18 23:51:35 -0500

    feat(nixos/tetuya/oom): slice and limit nix cores

--- nixos/tetuya/oom.nix
+++ nixos/tetuya/oom.nix
@@ -6,4 +6,16 @@
     freeSwapThreshold = 100;
     freeSwapKillThreshold = 100;
   };
+  # https://discourse.nixos.org/t/nix-build-ate-my-ram/35752
+  systemd = {
+    slices.nix-daemon.sliceConfig = {
+      ManagedOOMMemoryPressure = "kill";
+      ManagedOOMMemoryPressureLimit = "50%";
+    };
+    services.nix-daemon.serviceConfig = {
+      Slice = "nix-daemon.slice";
+      OOMScoreAdjust = 1000;
+    };
+  };
+  nix.settings.cores = 16;
 }
