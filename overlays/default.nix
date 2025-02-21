aff70e6b966d916037c751316b7d695d8c795196
Stephen Huan <stephen.huan@cgdct.moe>
2025-02-21 13:04:26 -0500

    Revert "fix(overlays/gpick): fix gpick build"
    This reverts commit ba24e3233462cd4bc8bdf554b2da005e75588fa0.

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,7 +5,6 @@
   caffeine-ng = import ./caffeine-ng;
   fishPlugins = import ./fishPlugins.nix;
   gcc' = import ./gcc.nix;
-  gpick = import ./gpick.nix;
   mpv = import ./mpv.nix;
   nix = import ./nix.nix;
   nobinsh = import ./nobinsh.nix;
