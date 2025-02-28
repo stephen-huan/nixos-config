1b0746761ea577a719c5f5aa74b407791bd90597
Stephen Huan <stephen.huan@cgdct.moe>
2025-02-27 22:09:07 -0500

    fix(overlays/tectonic): fix overlay and bump

--- overlays/tectonic.nix
+++ overlays/tectonic.nix
@@ -2,25 +2,29 @@ final: prev:
 
 let
   # https://discourse.nixos.org/t/4393/4
+  # https://github.com/NixOS/nixpkgs/pull/382550
+  # https://github.com/NixOS/nixpkgs/pull/194475
   tectonic-unwrapped = (
     final.tectonic-unwrapped.overrideAttrs { patches = [ ]; }
   ).override (previous: {
-    rustPlatform = previous.rustPlatform // {
-      buildRustPackage = args: previous.rustPlatform.buildRustPackage
-        (args // {
+    rustPlatform = previous.rustPlatform // rec {
+      buildRustPackage = previous.rustPlatform.buildRustPackage // {
+        __functor = _: args: previous.rustPlatform.buildRustPackage (args // {
           src = final.fetchFromGitHub {
             owner = "tectonic-typesetting";
             repo = "tectonic";
-            rev = "ddca0b805c7a8c3edd93effc2b188a05f409161d";
-            sha256 = "sha256-/hK3nRbrIq8X95REZonB/uVajc92bFMzRPauRTzYnh0=";
+            rev = "c2ae25ff1facd9e9cce31b48944b867773f709ec";
+            hash = "sha256-cDCJ1Tu2lA4KvN2EBUtT0MvMtkejd8YAoNRkNeoreEc=";
           };
-          cargoHash = "sha256-xN9LPlS3zcN0qW950pq0SN7G45xS/Gpi0DuIrqsd9L4=";
+          cargoHash = "sha256-dBthzRS+9wqKCwmo5cY/ynTdfIPK3QCsbZ2vAQ8q7aM=";
           cargoPatches = [ ];
           checkFlags = args.checkFlags or [ ] ++ [
             # permission denied (os error 13) while making cache root
             "--skip=tests::no_segfault_after_failed_compilation"
           ];
         });
+        override = _: buildRustPackage;
+      };
     };
   });
   self = prev.tectonic.override { inherit tectonic-unwrapped; };
