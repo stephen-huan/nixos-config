b98d13ab1fd682cb2a9793c44e0deafafff0196b
Stephen Huan <stephen.huan@cgdct.moe>
2025-02-27 22:10:33 -0500

    feat(nixos/_common/persistence): mount bind

--- nixos/_common/persistence.nix
+++ nixos/_common/persistence.nix
@@ -16,11 +16,17 @@ in
     hideMounts = true;
     directories = [
       { directory = "/tmp"; mode = "1777"; }
+      { directory = "/var/lib/bluetooth"; mode = "0700"; }
+      { directory = "/var/lib/iwd"; mode = "0700"; }
       "/var/cache"
       "/var/lib/nixos"
       "/var/lib/systemd"
       "/var/log"
-    ];
+    ] ++ lib.optional config.services.unbound.enable {
+      directory = config.services.unbound.stateDir;
+      mode = "0755";
+      inherit (config.services.unbound) user group;
+    };
     files = [ ];
     users.${config._module.args.username} = {
       directories = [
@@ -33,15 +39,9 @@ in
       ];
     };
   };
-  systemd.tmpfiles.settings."00-persistence" = persistPaths ({
-    "/var/lib/bluetooth" = { mode = "0700"; user = "root"; group = "root"; };
+  systemd.tmpfiles.settings."00-persistence" = persistPaths {
     "/var/lib/cups" = { mode = "0755"; user = "root"; group = "root"; };
-    "/var/lib/iwd" = { mode = "0700"; user = "root"; group = "root"; };
     "/var/lib/mullvad-vpn" = { mode = "0755"; user = "root"; group = "root"; };
     "/var/spool" = { mode = "0755"; user = "root"; group = "root"; };
-  } // lib.optionalAttrs config.services.unbound.enable {
-    "${config.services.unbound.stateDir}" = {
-      mode = "0755"; inherit (config.services.unbound) user group;
-    };
-  });
+  };
 }
