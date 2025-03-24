5184c77fe32d3a718c22577b629ff9ea27ecbddd
Stephen Huan <stephen.huan@cgdct.moe>
2025-03-24 02:57:55 -0400

    feat(overlays/sioyek): bump

--- overlays/sioyek.nix
+++ overlays/sioyek.nix
@@ -8,8 +8,8 @@ in
     src = final.fetchFromGitHub {
       owner = "ahrm";
       repo = "sioyek";
-      rev = "66880e1ebd9a81f6d80835f21d2309ea15d13ffd";
-      sha256 = "sha256-8W8a+YwK7C/qe9tIao6vXStnKN9NHBSFViXwuyhWazo=";
+      rev = "409efba2ded63182cebd2866af5cd9f03d5c9325";
+      sha256 = "sha256-lpPpQ95T/61oxirrjML75TxjeQHy18+QkxWHu0VrMus=";
     };
   };
 }
