aab654bb810dbe17fe68f81ae417a63b442a34aa
Stephen Huan <stephen.huan@cgdct.moe>
2025-05-24 18:42:33 -0400

    vimPlugins.lean-nvim: add lean-nvim

--- pkgs/applications/editors/vim/plugins/default.nix
+++ pkgs/applications/editors/vim/plugins/default.nix
@@ -2,6 +2,22 @@
 
 final: prev:
 {
+  lean-nvim = buildVimPlugin {
+    pname = "lean.nvim";
+    version = "2025-05-24";
+    src = fetchFromGitHub {
+      owner = "stephen-huan";
+      repo = "lean.nvim";
+      rev = "0e92b4d7b29d533cb5e5c99ccbcf5d76923b4f22";
+      sha256 = "sha256-iyBzp3XZgX2EI0oEe4VLNFi6n55HKHw5MjPv4ikbmx4=";
+    };
+    meta.homepage = "https://github.com/stephen-huan/lean.nvim/";
+    dependencies = with final; [
+      nvim-lspconfig
+      plenary-nvim
+    ];
+  };
+
   polar-nvim = buildVimPlugin {
     pname = "polar.nvim";
     version = "2024-06-08";
