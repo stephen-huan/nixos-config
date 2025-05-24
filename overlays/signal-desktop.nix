f7cd069f77fa7cc75a010aa27b000af91f49dcac
Stephen Huan <stephen.huan@cgdct.moe>
2025-05-24 18:37:57 -0400

    fix(overlays/signal-desktop): use source build

--- overlays/signal-desktop.nix
+++ overlays/signal-desktop.nix
@@ -1,35 +1,15 @@
 final: prev:
 
 let
-  self = prev.signal-desktop;
+  self = prev.signal-desktop.override {
+    withAppleEmojis = true;
+  };
 in
 {
   # revert https://github.com/NixOS/nixpkgs/pull/337161
   signal-desktop = self.overrideAttrs (previousAttrs: {
-    src = final.fetchurl {
-      url = builtins.replaceStrings [ self.version ] [ "7.46.0" ]
-        previousAttrs.src.url;
-      hash = "sha256-busAYYdxrXhezvqm9p5lbeIG5OMQUWg4RKu2p7ME0D4=";
+    meta = previousAttrs.meta // {
+      license = final.lib.dropEnd 1 previousAttrs.meta.license;
     };
-
-    nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ final.dpkg ];
-
-    unpackPhase = ''
-      runHook preUnpack
-
-      dpkg-deb -x $src .
-
-      runHook postUnpack
-    '';
-
-    installPhase =
-      let
-        lines = final.lib.splitString "\n" previousAttrs.installPhase;
-        line =
-          "# Copy the Noto Color Emoji PNGs into the ASAR contents. See `src`";
-        index = final.lib.lists.findFirstIndex (x: x == line) null lines;
-      in
-      builtins.concatStringsSep "\n"
-        ((final.lib.take index lines) ++ [ "runHook postInstall" ]);
   });
 }
