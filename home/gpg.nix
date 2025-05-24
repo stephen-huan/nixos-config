14bc47305fe6b8d6ce8448ab5ef728ad6ce16598
Stephen Huan <stephen.huan@cgdct.moe>
2025-05-24 18:38:22 -0400

    refactor(home/gpg): pinentryPackage -> pinentry.package

--- home/gpg.nix
+++ home/gpg.nix
@@ -5,7 +5,7 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentryPackage = pkgs.pinentry-qt;
+    pinentry.package = pkgs.pinentry-qt;
     # 30 minutes, resets with usage
     defaultCacheTtl = 1800;
     # 2 hours, maximum
