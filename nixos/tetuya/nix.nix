ac05ae181c25d82641c32312bc55ca5d014bdf66
Stephen Huan <stephen.huan@cgdct.moe>
2025-10-07 07:18:38 -0400

    feat(tetuya/nix): add nix-community, flox caches

---
+++ nixos/tetuya/nix.nix
@@ -0,0 +1,14 @@
+{
+  nix.settings = {
+    # https://nix-community.org/cache/
+    # https://flox.dev/blog/the-flox-catalog-now-contains-nvidia-cuda/
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.flox.dev"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+    ];
+  };
+}
