0ea2b669712e698ed572cc659ff158c166e8408a
Stephen Huan <stephen.huan@cgdct.moe>
2025-10-07 07:31:10 -0400

    fix(nixos/tetuya/nix): lower flox cache priority

--- nixos/tetuya/nix.nix
+++ nixos/tetuya/nix.nix
@@ -4,7 +4,7 @@
     # https://flox.dev/blog/the-flox-catalog-now-contains-nvidia-cuda/
     substituters = [
       "https://nix-community.cachix.org"
-      "https://cache.flox.dev"
+      "https://cache.flox.dev?priority=42"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
