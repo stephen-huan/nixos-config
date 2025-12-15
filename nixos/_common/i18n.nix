a161141de01548e118bac8e0491bf9918a251a30
Stephen Huan <stephen.huan@cgdct.moe>
2025-12-15 09:26:21 -0500

    refactor(nixos/i18n): supportedLocales -> extraLocales

--- nixos/_common/i18n.nix
+++ nixos/_common/i18n.nix
@@ -1,9 +1,9 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    supportedLocales = lib.mkOptionDefault [ "ja_JP.UTF-8/UTF-8" ];
+    extraLocales = [ "ja_JP.UTF-8/UTF-8" ];
     inputMethod = {
       enable = true;
       type = "ibus";
