e1b16f689413d4392aabda8bbb4ce9e9726f85bc
Stephen Huan <stephen.huan@cgdct.moe>
2025-12-22 08:41:14 -0500

    refactor(nvim/leap-nvim): add_default_mappings()

--- home/neovim/nvim/lua/plugins/leap-nvim.lua
+++ home/neovim/nvim/lua/plugins/leap-nvim.lua
@@ -1,2 +1,6 @@
 -- keybindings
-require("leap").add_default_mappings()
+vim.keymap.set({ "n", "x", "o" }, "s", "<plug>(leap-forward)")
+vim.keymap.set({ "n", "x", "o" }, "S", "<plug>(leap-backward)")
+vim.keymap.set("n", "gs", "<plug>(leap-from-window)")
+vim.keymap.set({ "x", "o" }, "x", "<plug>(leap-forward-till)")
+vim.keymap.set({ "x", "o" }, "X", "<plug>(leap-backward-till)")
