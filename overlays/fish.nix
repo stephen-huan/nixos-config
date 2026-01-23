00d28e1dd1595b44a0edcf3261881f8fc50a7b6b
Stephen Huan <stephen.huan@cgdct.moe>
2026-01-22 23:05:36 -0500

    fix(overlays/fish): simplify binsh substitution

--- overlays/fish.nix
+++ overlays/fish.nix
@@ -6,13 +6,12 @@ let
 in
 {
   fish = self.overrideAttrs (previousAttrs: {
+    # https://github.com/fish-shell/fish-shell/commit/7ca78e717892c56efa75b16a489075d6c30033c7
     # https://github.com/fish-shell/fish-shell/commit/e015956de721ef8e67f4d069ee46c28ef71f5744
     postPatch = previousAttrs.postPatch or "" + ''
       substituteInPlace \
-        share/functions/__fish_apropos.fish \
-        share/functions/fish_git_prompt.fish \
-        share/functions/fish_update_completions.fish \
-        share/functions/help.fish \
+        share/functions/__fish_posix_shell.fish \
+        src/fork_exec/mod.rs \
         --replace-fail "/bin/sh" "${sh}"
     '';
   });
