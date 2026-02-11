7eb2cc24c37b97c559f5750d6b64e1b0f720ff72
Stephen Huan <stephen.huan@cgdct.moe>
2026-02-11 09:47:24 -0500

    refactor(home/nvim/lean-nvim): use vim.lsp.config

--- home/neovim/nvim/lua/plugins/lean-nvim.lua
+++ home/neovim/nvim/lua/plugins/lean-nvim.lua
@@ -1,6 +1,6 @@
 local on_attach = require("config.lsp").on_attach
+vim.lsp.config("leanls", { on_attach = on_attach })
 require("lean").setup {
-    lsp = { on_attach = on_attach },
     abbreviations = { enable = true },
     mappings = true,
     infoview = { autoopen = true },
