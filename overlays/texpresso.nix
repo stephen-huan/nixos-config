0512a5e58c5c549f64cc4bef5b90f82631ac0f4d
Stephen Huan <stephen.huan@cgdct.moe>
2026-04-18 06:06:53 -0400

    feat(overlays/texpresso): bump

--- overlays/texpresso.nix
+++ overlays/texpresso.nix
@@ -4,28 +4,22 @@ let
   self = prev.texpresso;
 in
 {
-  # https://github.com/NixOS/nixpkgs/pull/392621
+  # https://github.com/NixOS/nixpkgs/pull/509216
   texpresso = self.overrideAttrs (previousAttrs: {
     src = final.fetchFromGitHub {
       owner = "let-def";
       repo = "texpresso";
-      rev = "6073a336dd67e5d4fde0ded4b33580920de7f69d";
-      sha256 = "sha256-J+1OtrLUP9P3diUcndlQnXp70dfjAfc39ZYcIVr5/hI=";
+      rev = "a7c8050116ce5e61dbc684df127dfe2875bdbd4d";
+      hash = "sha256-eGrk8PU3/+f8YRj7eLS8im/EOagLISpuc5LGTHfzwA0=";
     };
-    passthru = previousAttrs.passthru // {
-      tectonic = previousAttrs.passthru.tectonic.overrideAttrs (super: {
-        src = final.fetchFromGitHub {
-          owner = "let-def";
-          repo = "tectonic";
-          rev = "be0a9543600ab3a98f6ae6c37047522c09c2a02e";
-          hash = "sha256-jpAVG1xzpAh9Y6d3uSRo6OHuLWwGnkU8PraZGjqmNY4=";
-          fetchSubmodules = true;
-        };
-        cargoPatches = [ ];
-        patches = final.lib.drop ((builtins.length super.cargoPatches) + 2)
-          super.patches;
-        cargoHash = "sha256-mX9DsucLbls1w0ULQ6kEHel/u1PZjQPcGZK3pjN7RVE=";
-      });
-    };
+    nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [
+      final.fontconfig
+      final.icu
+      final.pkg-config
+    ];
+    buildFlags = [ "texpresso" "texpresso-xetex" ];
+    postInstall = ''
+      install -Dm0755 -t "$out/bin/" "build/texpresso-xetex"
+    '';
   });
 }
