e63260fe728741d02f6d75491b1dc2ccee33b40f
Stephen Huan <stephen.huan@cgdct.moe>
2026-05-03 23:03:54 -0400

    fix(nixos/boot): disable debug shell, systemd

--- nixos/_common/boot.nix
+++ nixos/_common/boot.nix
@@ -17,7 +17,8 @@ let
 in
 {
   boot = {
-    kernelParams = [ "boot.shell_on_fail" ];
+    # debug shell on tty9 (ctrl+alt+f9)
+    kernelParams = lib.mkIf false [ "rd.systemd.debug_shell" ];
     loader = {
       efi.canTouchEfiVariables = false;
       systemd-boot = {
