d5a04224bf921c3772004c1758914d4b8791e0e2
Stephen Huan <stephen.huan@cgdct.moe>
2026-05-06 02:09:39 -0400

    feat(nixos/systemd): add /usr patches

--- nixos/_common/systemd/default.nix
+++ nixos/_common/systemd/default.nix
@@ -5,6 +5,8 @@ let
     patches = previousAttrs.patches or [ ] ++ [
       ./0001-core-namespace-support-ProtectSystem-without-usr.patch
       ./0002-update-done-default-to-current-time-without-usr.patch
+      ./0003-core-main-run-if-usr-is-not-populated.patch
+      ./0004-base-filesystem-avoid-creating-bin-and-usr.patch
     ];
   });
 in
