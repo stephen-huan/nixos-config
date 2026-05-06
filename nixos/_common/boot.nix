965538820eee13cd8014104c37bc5e12279a67ba
Stephen Huan <stephen.huan@cgdct.moe>
2026-05-06 02:13:10 -0400

    refactor(nixos/boot): remove ad-hoc /usr services
    This reverts commit 47a623f9b4a3b57fd042ffe840a2ef171df4da5d.

--- nixos/_common/boot.nix
+++ nixos/_common/boot.nix
@@ -1,20 +1,5 @@
-{ config, lib, pkgs, options, ... }:
+{ lib, ... }:
 
-let
-  cfg = config.boot.initrd.systemd;
-  usrbinenv =
-    if config.environment.usrbinenv != null
-    then config.environment.usrbinenv
-    else options.environment.usrbinenv.default;
-  script = pkgs.writeShellScript "usrbinenv" ''
-    export PATH="${pkgs.coreutils}/bin"
-
-    mkdir -p /usr/bin
-    chmod 0755 /usr/bin
-    ln -sfn ${usrbinenv} /usr/bin/.env.tmp
-    mv /usr/bin/.env.tmp /usr/bin/env # atomically replace /usr/bin/env
-  '';
-in
 {
   boot = {
     # debug shell on tty9 (ctrl+alt+f9)
@@ -26,40 +11,5 @@ in
         editor = false;
       };
     };
-    # work around systemd initrd not supporting no /usr
-    # - https://github.com/NixOS/nixpkgs/pull/435781
-    # - https://systemd.io/INITRD_INTERFACE/
-    # - https://systemd.io/SEPARATE_USR_IS_BROKEN/
-    initrd.systemd.services = {
-      initrd-nixos-activation-post = {
-        after = [ "initrd-nixos-activation.service" ];
-        requiredBy = [ "initrd-switch-root.service" ];
-        before = [ "initrd-switch-root.service" ];
-        unitConfig.DefaultDependencies = false;
-        serviceConfig.Type = "oneshot";
-
-        # see nixos/modules/system/boot/systemd/initrd.nix
-        script = ''
-          set -uo pipefail
-          export PATH="/bin:${cfg.package.util-linux}/bin"
-          exec chroot /sysroot "${script}"
-        '';
-      };
-    };
-  };
-  systemd.services.initrd-nixos-activation-post-post = {
-    wantedBy = [ "local-fs-pre.target" ];
-    before = [ "local-fs-pre.target" ];
-    unitConfig.DefaultDependencies = false;
-    serviceConfig.Type = "oneshot";
-
-    script = ''
-      if test -L /bin; then rm -f /bin; fi
-
-      rm -f /usr/bin/env
-      if test -d /usr/bin; then rmdir --ignore-fail-on-non-empty /usr/bin; fi
-      if test -d /usr; then rmdir --ignore-fail-on-non-empty /usr; fi
-      # HACK: prevent garbage collection ${script}
-    '';
   };
 }
