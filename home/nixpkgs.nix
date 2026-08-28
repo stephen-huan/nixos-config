1574596c1c918a730a76ee43a7eb33ecee84ca99
Stephen Huan <stephen.huan@cgdct.moe>
2026-08-27 23:28:38 -0400

    fix(home/nixpkgs): disable if useGlobalPkgs

--- home/nixpkgs.nix
+++ home/nixpkgs.nix
@@ -1,11 +1,11 @@
-{ lib, pkgs, self, ... }:
+{ lib, pkgs, osConfig, self, ... }:
 
 let
   unfreePkgs = map lib.getName (import ./home-manager/unfree.nix pkgs);
   permittedInsecurePackages = import ./home-manager/insecure.nix pkgs;
 in
 {
-  nixpkgs = {
+  nixpkgs = lib.mkIf (!osConfig.home-manager.useGlobalPkgs) {
     overlays = builtins.attrValues self.overlays;
     config = {
       allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) unfreePkgs;
