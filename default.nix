79afbb95ded02690be759520e9e1047a126a86e3
Stephen Huan <stephen.huan@cgdct.moe>
2026-08-27 23:27:50 -0400

    style(default.nix): remove comments

--- default.nix
+++ default.nix
@@ -1,15 +1,6 @@
-# This file describes your repository contents.
-# It should return a set of nix derivations
-# and optionally the special attributes `lib`, `modules` and `overlays`.
-# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
-# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
-# commands such as:
-#     nix-build -A mypackage
-
 { pkgs }:
 
 (import ./pkgs/top-level { inherit pkgs; }) // (import ./modules) // {
-  # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
-  overlays = import ./overlays; # nixpkgs overlays
+  lib = import ./lib { inherit pkgs; };
+  overlays = import ./overlays;
 }
