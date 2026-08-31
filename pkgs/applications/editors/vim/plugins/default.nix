31aa43274707fdd559cfb59de8688a2ea601d36c
Stephen Huan <stephen.huan@cgdct.moe>
2026-08-30 22:32:44 -0400

    vimPlugins: update version, sha256 -> hash

--- pkgs/applications/editors/vim/plugins/default.nix
+++ pkgs/applications/editors/vim/plugins/default.nix
@@ -20,24 +20,24 @@ final: prev:
 
   polar-nvim = buildVimPlugin {
     pname = "polar.nvim";
-    version = "2025-05-24";
+    version = "0-unstable-2025-05-24";
     src = fetchFromGitHub {
       owner = "stephen-huan";
       repo = "polar.nvim";
       rev = "6ccccce72763bb0c45d361c2c59e729f24a852aa";
-      sha256 = "sha256-djzJqW+nElJpkAPfjmVJsli+fWd8da0rzdSnB9CcOfY=";
+      hash = "sha256-djzJqW+nElJpkAPfjmVJsli+fWd8da0rzdSnB9CcOfY=";
     };
     meta.homepage = "https://github.com/stephen-huan/polar.nvim/";
   };
 
   vim-cython-syntax = buildVimPlugin {
     pname = "vim-cython-syntax";
-    version = "2023-08-02";
+    version = "0-unstable-2023-08-02";
     src = fetchFromGitHub {
       owner = "lambdalisue";
       repo = "vim-cython-syntax";
       rev = "17a873c8cc026da4460c1ee1aa12709358db8360";
-      sha256 = "sha256-5Qvpyojn7B784Gpw5XgSgXTMla3POnIA4xHUcdkq6Ks=";
+      hash = "sha256-5Qvpyojn7B784Gpw5XgSgXTMla3POnIA4xHUcdkq6Ks=";
     };
     meta.homepage = "https://github.com/lambdalisue/vim-cython-syntax/";
   };
