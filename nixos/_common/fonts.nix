42fd9213d78909fb436b38a726e44bf6345cd25d
Stephen Huan <stephen.huan@cgdct.moe>
2026-08-30 23:35:55 -0400

    fix(nixos/_common/fonts): set mono font order

--- nixos/_common/fonts.nix
+++ nixos/_common/fonts.nix
@@ -4,7 +4,7 @@
     defaultFonts = {
       sansSerif = [ "Noto Sans" ];
       serif = [ "Noto Serif" ];
-      monospace = [ "Noto Sans Mono" ];
+      monospace = [ "Noto Sans Mono" "DejaVu Sans Mono" "Noto Color Emoji" ];
       emoji = [ "Noto Color Emoji" ];
     };
   };
