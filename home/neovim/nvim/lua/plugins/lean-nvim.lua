ff83c4df9707b70b4ba494993b7ed8af6d735e65
Stephen Huan <stephen.huan@cgdct.moe>
2026-08-30 22:39:27 -0400

    refactor(nvim/lean-nvim): integrate plugin changes

--- home/neovim/nvim/lua/plugins/lean-nvim.lua
+++ home/neovim/nvim/lua/plugins/lean-nvim.lua
@@ -1,17 +1,23 @@
 local on_attach = require("config.lsp").on_attach
 vim.lsp.config("leanls", { on_attach = on_attach })
-require("lean").setup {
+vim.g.lean_config = {
     abbreviations = { enable = true },
     mappings = true,
-    infoview = { autoopen = true },
+    infoview = {
+        autoopen = true,
+        -- leanprover/verso (bisected 41b85d429fcdb33349115edc063078dd98ee7c0b)
+        -- see src/verso/Verso/Code/Highlighted.lean
+        messages = {
+            goals = {
+                accomplished = string.lower "All goals completed! 🐙",
+                none = string.lower "No goals.",
+            },
+        },
+    },
     goal_markers = {
         -- gets in the way of writing proofs
         unsolved = "",
         -- "" disables goals entirely
         accomplished = " ",
-        -- leanprover/verso (bisected 41b85d429fcdb33349115edc063078dd98ee7c0b)
-        -- see src/verso/Verso/Code/Highlighted.lean
-        goals_accomplished = string.lower "All goals completed! 🐙",
-        no_goals = string.lower "No goals.",
     },
 }
